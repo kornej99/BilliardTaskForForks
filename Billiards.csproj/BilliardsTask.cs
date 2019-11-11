9035688075c0fcdb7914759c7a0fde6683b9958e
kornej99 <56335230+kornej99@users.noreply.github.com>
2019-11-11 08:57:40 +0400

    added method

--- Billiards.csproj/BilliardsTask.cs
+++ Billiards.csproj/BilliardsTask.cs
@@ -4,7 +4,7 @@ namespace Billiards
 {
     public static class BilliardsTask
     {
-        /// <summary>
+        /// <summary>   
         /// 
         /// </summary>
         /// <param name="directionRadians">Угол направелния движения шара</param>
@@ -12,8 +12,23 @@ namespace Billiards
         /// <returns></returns>
         public static double BounceWall(double directionRadians, double wallInclinationRadians)
         {
-            //TODO
-            return 0.0;
+
+            double direction = (directionRadians * 180) / Math.PI;
+            double wallInclination = (wallInclinationRadians * 180) / Math.PI;
+
+            double wallAngle = 90 + wallInclination;
+            double ballAngle = 180 + direction;
+
+            double diff = wallAngle - ballAngle;
+            double angle = ballAngle + 2 * diff + 360 * 5;
+
+            angle %= 360;
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+
+            return (angle * Math.PI) / 180;
+        }
         }
     }
-}
